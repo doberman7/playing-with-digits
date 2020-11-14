51a22da6247dcf92a5cb2dae685da7dd7a39627f
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-14 12:53:39 -0600

    doone

--- playing-with-digits.js
+++ playing-with-digits.js
@@ -1,8 +1,8 @@
 //Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
-digPow(89, 1) //should return 1 since 8¹ + 9² = 89 = 89 * 1
+// digPow(89, 1) //should return 1 since 8¹ + 9² = 89 = 89 * 1
 digPow(92, 1) //should return -1 since there is no k such as 9¹ + 2² equals 92 * k
-digPow(695, 2) //should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
-digPow(46288, 3) //should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+// digPow(695, 2) //should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+// digPow(46288, 3) //should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 
 
 function digPow(n, p) {
@@ -15,9 +15,9 @@ function digPow(n, p) {
   }
   //usar Math. floor() para obtener la parte entera de SUMA y restársela al SUMA y si el resultado es 0 el número es entero
 
-  console.log( Math.floor(suma)-suma=== 0 ? suma/n  : -1 );
+  console.log( (Math.floor(suma))-suma=== 0 && (Math.floor(suma))-suma > 0 ? suma/n  : -1);
 
 
-
+  //
   //si la suma de las elevaciones es entero regrersar 1
 }
